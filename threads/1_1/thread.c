22ae90eec373c0f3a9acbcadb68644f0c7f2b4a0
ArtiomVlasov <artiom.lvlasov@gmail.com>
2025-09-20 05:47:50 +0000

    .

--- threads/1_1/thread.c
+++ threads/1_1/thread.c
@@ -26,11 +26,6 @@ int main()
 		return -1;
 	}
 
-	err = pthread_join(err, NULL);
-	if (err) {
-      perror("main: pthread joining error");
-      return -1;
-    }
-	
-	return 0;
+	pthread_exit(NULL);
+
 }
